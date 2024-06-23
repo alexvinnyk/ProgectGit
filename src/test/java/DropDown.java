f3990a576311f204a1d788d753202c6ff6a053d4
Alex <alexvinnyk@yahoo.com>
2024-06-22 20:14:22 -0500

    dropDown

---
+++ src/test/java/DropDown.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DropDown {
+   /*
+    Select something = new Select(driver.getDriver);
+    List<WebElement> web = something.getOption();
+    List<String > actualStr = new ArrayList<>();
+    for(WebElement each : web){
+     actualStr.add(each.getText());
+    Assert.assertEquals(expected,actualStr);
+
+} */
+}
