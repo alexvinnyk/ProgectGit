74526cb294e1fd22f9bad29b1032c01ad783408c
Alex <alexvinnyk@yahoo.com>
2024-07-29 22:03:25 -0500

    data added

--- src/test/java/Overloading.java
+++ src/test/java/Overloading.java
@@ -2,6 +2,8 @@ public class Overloading {
     public static void main(String[] args) {
         int sum1 = sum(1, 5);
         System.out.println("sum1 = " + sum1);
+        int sum2 = sum(5, 5, 5);
+        System.out.println("sum2 = " + sum2);
     }
 
 public static int sum (int a,int b){
